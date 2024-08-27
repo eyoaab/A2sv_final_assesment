db3bfb2e660542331052d826330c623e19281d1d
eyoaab <eyobtariku48@gmail.com>
2024-08-27 17:56:55 +0300

    completed the fiven future

--- grocery_app/lib/features/Grosary/Presentation/Pages/HomePage/home_page.dart
+++ grocery_app/lib/features/Grosary/Presentation/Pages/HomePage/home_page.dart
@@ -20,6 +20,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: BlocBuilder<GroceryBloc, GroceryState>(
