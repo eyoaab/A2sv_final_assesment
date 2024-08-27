17bd5ce3faa8526faf4e8b98d9a95cb49c59f2ba
eyoaab <eyobtariku48@gmail.com>
2024-08-27 09:56:44 +0300

    added thee entity

---
+++ grocery_app/lib/feutures/Grosary/Domein/Entity/OptionEntity.dart
@@ -0,0 +1,13 @@
+class OptionEntity{
+
+  final String id;
+  final String name;
+  final double price;
+
+  OptionEntity({
+    required this.id,
+    required this.name,
+    required this.price,
+
+  });
+}
---
+++ grocery_app/lib/feutures/Grosary/Domein/Entity/grocery_entity.dart
@@ -0,0 +1,24 @@
+import './OptionEntity.dart';
+// Grocery Item Class
+class GroceryEntity {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final double rating;
+  final double price;
+  final double discount;
+  final String description;
+  final List<OptionEntity> options;
+
+
+  GroceryEntity({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.rating,
+    required this.price,
+    required this.discount,
+    required this.description,
+    required this.options,
+  });
+}
